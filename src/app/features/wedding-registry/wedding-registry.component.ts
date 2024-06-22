2ccfd0dc92f80228b3a0cb9159e5e00b65491dc0
Sk Masum <masum0aktar@gmail.com>
2024-06-22 14:30:44 +0530

    wedding registry done

--- src/app/features/wedding-registry/wedding-registry.component.ts
+++ src/app/features/wedding-registry/wedding-registry.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { RadioGroupComponent } from './radio-group/radio-group.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-wedding-registry',
   standalone: true,
-  imports: [],
+  imports: [RadioGroupComponent, ReactiveFormsModule],
   templateUrl: './wedding-registry.component.html',
-  styleUrl: './wedding-registry.component.scss'
+  styleUrl: './wedding-registry.component.scss',
 })
 export class WeddingRegistryComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      name: [],
+      email: [],
+      isAttending: [],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
